5497e528b97a5e5b8a8e8839dfc3885e1a1247ea
AgreeableAK <anujsehajwal@gmail.com>
2026-06-01 22:16:36 +0530

    Two function to find the start of the transaction table and clearing the Header

--- statement_cleaner.py
+++ statement_cleaner.py
@@ -15,7 +15,59 @@ def load_statement(file_path):
         print(f"Unexpected error loading file: {e}")
         return None
     
+
+def find_table_start(df):
+
+    keywords = ["date","s no.","cheque", "description", "amount", "balance", "withdrawal", "deposit", "transaction"]
+
+    for index, row in df.head(50).iterrows():
+
+        match_count = 0
+
+        for cell in row:
+
+            if pd.isna(cell):
+                continue
+
+            cell_text = str(cell).lower()
+
+            for keyword in keywords:
+                if keyword in cell_text:
+                    match_count += 1
+                    break
+
+        if match_count >= 5:
+            print(f"Table header found at row index: {index}")
+            return index
+    print(f"Error: Could not find start of transaction table. Please check the file format.")
+
+
+def extract_table(df, start_index):
+
+    cleaned_df = df.iloc[start_index:].copy()
+
+    cleaned_df.columns = cleaned_df.iloc[0]
+
+    cleaned_df = cleaned_df[1:]
+    cleaned_df = cleaned_df.reset_index(drop=True)
+
+    return cleaned_df
+
 if __name__ == "__main__":
 
     file_path = Path("~\\Documents\\Projects\\bank-statement-cleaner\\OpTransactionHistory29-05-2026(Redacted).xls")
     df = load_statement(file_path)
+
+    raw_data = load_statement(file_path)
+
+
+    if raw_data is not None:
+    
+        start_row = find_table_start(raw_data)
+        
+        if start_row is not None:
+            transaction_table = extract_table(raw_data, start_row)
+            
+            # Print the first 5 rows to verify it worked!
+            print("\nExtracted Table Preview:")
+            print(transaction_table.head()) 
